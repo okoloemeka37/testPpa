fe83a54624638e280866b6b1ea41049e14c37d60
okoloemeka37 <97386408+okoloemeka37@users.noreply.github.com>
2024-11-23 03:56:19 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaystackController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('callback',PaystackController::class,'callback')->name('callback');
+//Route::get('success',PaystackController::class,'success')->name('success');
+//Route::get('cancel',PaystackController::class,'cancel')->name('cancel');
+
+
+Route::post('/pay', [PaystackController::class, 'redirectToGateway'])->name('pay');
+
+
+Route::get('/payment/callback', [PaystackController::class, 'handleGatewayCallback']);
